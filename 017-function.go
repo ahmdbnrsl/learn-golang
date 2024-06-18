a45349ff7ad72ccc90cf19147d35de7a9146bf90
abrazax56 <ahmadbenirusli@gmail.com>
2024-06-18 08:15:09 +0700

    function: variadic function, slice to vararg, function value, function as parameter, type declaration for function

--- 017-function.go
+++ 017-function.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func say() {
     println("Beni")
 }
@@ -28,6 +30,43 @@ func Name() (firstName, lastName string) {
     return firstName, lastName
 }
 
+//variadic function
+func sumAll(numbers ...int) int {
+    total := 0
+    for _, number := range numbers {
+        total += number
+    }
+    return total 
+}
+
+//Function value
+func sayGoodBye(name string) string {
+    return "Good bye " + name
+}
+
+//function as parameter
+func plus(numbers ...int) int {
+    hasil := 0
+    for _, number := range numbers {
+        hasil += number
+    }
+    return hasil
+}
+func kali(numbers ...int) int {
+    hasil := 1
+    for _, number := range numbers {
+        hasil *= number
+    }
+    return hasil
+}
+
+type Math func(numbers ...int) int // type declaration
+
+func math(plus, kali Math, numbers ...int) (int, int) {
+    fmt.Println(numbers)
+    return plus(numbers...), kali(numbers...)
+}
+
 func main() {
     say()
     sayName("Via Fitriana", 20)
@@ -40,7 +79,22 @@ func main() {
     println(kurang)
     tambah, _ := operator(30, 20)
     println(tambah)
-    
+    //Named return value
     firstName, lastName := Name()
     println(firstName, lastName)
+    //variadic function
+    println(sumAll(1, 2, 3, 4, 5))
+    //convert slice to vararg parameter 
+    isSlice := []int{
+        20,
+        50,
+        50,
+    }
+    hasil := sumAll(isSlice...) // like spread operator in javascript:v
+    println(hasil)
+    //Function value
+    goodBye := sayGoodBye
+    println(goodBye("Beni"))
+    //Function as parameter
+    println(math(plus, kali, 10, 10))
 }
