c8a95eaf09fe8dbc201c2ef3cc4450ee1cb4c732
abrazax56 <ahmadbenirusli@gmail.com>
2024-07-17 14:22:49 +0700

    go-routines: send & recieve only channel, buffered channel, select keyword

--- go-routines/test/chanel_test.go
+++ go-routines/test/chanel_test.go
@@ -50,3 +50,69 @@ func TestAsyncFunction(t *testing.T) {
     b := pow(10)
     fmt.Println(<- a, <-b)
 }
+
+//=== SEND AND RECEIVE ONLY CHANNEL
+
+//send only
+func doSomething(c chan<- string) {
+    go func(){
+        c <- "Hello World"
+    }()
+}
+//receive only
+func printChannel(c <-chan string) {
+    fmt.Println(<- c)
+}
+
+func TestSendAndReceiveOnlyChannel(t *testing.T) {
+    channels := make(chan string)
+    doSomething(channels)
+    printChannel(channels)
+}
+
+//buffered channel
+
+func TestBufferedChannel(t *testing.T) {
+    chans := make(chan string, 3)
+    chans <- "Ahmad Beni Rusli"
+    chans <- "Xaviera Putri"
+    chans <- "Via Fitriana"
+    //chans <- "Suko"  Deadlock because out of capacity 
+    fmt.Println(<- chans)
+    fmt.Println(<- chans)
+    fmt.Println(<- chans)
+}
+
+// Select keyword
+
+func TestSelect(t *testing.T) {
+    c1 := make(chan int)
+    c2 := make(chan int)
+    doneChannel := make(chan int)
+    
+    go func() {
+        time.Sleep(4 * time.Second)
+        c1 <- 1
+    }()
+    
+    go func() {
+        time.Sleep(3 * time.Second)
+        c2 <- 2
+    }()
+    
+    go func() {
+        time.Sleep(5 * time.Second)
+        doneChannel <- 0
+    }()
+    
+    for {
+        select {
+        case data := <- c1:
+            fmt.Println(data)
+        case data := <- c2:
+            fmt.Println(data)
+        case <- doneChannel:
+            return
+        }
+    }
+}
