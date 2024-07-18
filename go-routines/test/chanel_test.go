8561b17f80764a5068dcf3602e26b3a96f1fe42e
abrazax56 <ahmadbenirusli@gmail.com>
2024-07-18 14:23:56 +0700

    go-routines: iterate channel, default channel select, wait group

--- go-routines/test/chanel_test.go
+++ go-routines/test/chanel_test.go
@@ -3,6 +3,7 @@ package test
 import "fmt"
 import "testing"
 import "time"
+import "strconv"
 
 func TestChannel(t *testing.T) {
     
@@ -104,15 +105,64 @@ func TestSelect(t *testing.T) {
         time.Sleep(5 * time.Second)
         doneChannel <- 0
     }()
-    
+   
+    counter := 0
     for {
         select {
         case data := <- c1:
             fmt.Println(data)
+            counter++
         case data := <- c2:
             fmt.Println(data)
-        case <- doneChannel:
-            return
+            counter++
         }
+        if counter == 2 {
+			break
+		}
     }
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go doSomething(channel1)
+	go doSomething(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
 }
