2ed36f2047ef8fef6721f26bb63847be56ed7ba7
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-03 07:43:48 +0700

    go-web: template [template data, if, comparison operator, range, with]

--- go-web/template_test.go
+++ go-web/template_test.go
@@ -54,3 +54,124 @@ func Test20TemplateFiles(t *testing.T) {
     body, _ := io.ReadAll(response.Body)
     fmt.Println(string(body))
 }
+type Page struct {
+    Title string
+    Name string
+}
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+    t := template.Must(template.ParseFiles("./template/name.gohtml"))
+    // USING MAP
+    /*t.ExecuteTemplate(writer, "name.gohtml", map[string]any{
+        Title: "Via",
+        Name: "Fitriana",
+    })*/
+    t.ExecuteTemplate(writer, "name.gohtml", Page{
+        Title: "Via",
+        Name: "Fitriana",
+    })
+}
+
+func Test21TemplateDataStruct(t *testing.T) {
+    request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+    recorder := httptest.NewRecorder()
+    
+    TemplateDataStruct(recorder, request)
+    
+    response := recorder.Result()
+    body, _ := io.ReadAll(response.Body)
+    fmt.Println(string(body))
+}
+// type Person struct {
+//     Name string
+// }
+func TemplateIfStatement(writer http.ResponseWriter, request *http.Request) {
+    t := template.Must(template.ParseFiles("./template/if.gohtml"))
+    person := struct{
+        Name string
+    }{"Via Fitriana"}
+    t.ExecuteTemplate(writer, "if.gohtml", person)
+}
+
+func Test22TemplateIfStatement(t *testing.T) {
+    request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+    recorder := httptest.NewRecorder()
+    
+    TemplateIfStatement(recorder, request)
+    
+    response := recorder.Result()
+    body, _ := io.ReadAll(response.Body)
+    fmt.Println(string(body))
+}
+
+//===== OPERATOR PERBANDINGAN======
+/*
+eq artinya arg1 == arg2
+ne artinya arg1 !== arg2
+lt  artinya arg1 < arg2
+le artinya arg1 <= arg2
+gt artinya arg1 > arg2
+ge artinya arg1 >= arg2
+*/
+
+func TemplateIfComparison(writer http.ResponseWriter, request *http.Request) {
+    t := template.Must(template.ParseFiles("./template/perbandingan.gohtml"))
+    nilai := struct{
+        Value int
+    }{90}
+    t.ExecuteTemplate(writer, "perbandingan.gohtml", nilai)
+}
+
+func Test23TemplateIfComparison(t *testing.T) {
+    request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+    recorder := httptest.NewRecorder()
+    
+    TemplateIfComparison(recorder, request)
+    
+    response := recorder.Result()
+    body, _ := io.ReadAll(response.Body)
+    fmt.Println(string(body))
+}
+
+/* MENGAPA OPERATOR NYA DI DEPAN, KARENA ITU SAMA HALNYA MEMANGGIL FUNCTION 
+
+MISAL ge arg1 80 maka ge(arg1, 80)
+*/
+
+func TemplateRange(writer http.ResponseWriter, request *http.Request) {
+    t := template.Must(template.ParseFiles("./template/range.gohtml"))
+    fav := struct{
+        Favourite []string
+    }{[]string{"Via", "Fitirana", "Via Fitriana"}}
+    t.ExecuteTemplate(writer, "range.gohtml", fav)
+}
+func Test24TemplateRange(t *testing.T) {
+    request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+    recorder := httptest.NewRecorder()
+    
+    TemplateRange(recorder, request)
+    
+    response := recorder.Result()
+    body, _ := io.ReadAll(response.Body)
+    fmt.Println(string(body))
+}
+
+func TemplateWith(writer http.ResponseWriter, request *http.Request) {
+    t := template.Must(template.ParseFiles("./template/with.gohtml"))
+    type Addr struct{ Street, District string }
+    person := struct{
+        Name string
+        Address Addr
+    }{"Via Fitriana", Addr{"jln surga", "Sidareja"}}
+    t.ExecuteTemplate(writer, "with.gohtml", person)
+}
+
+func Test25TemplateWith(t *testing.T) {
+    request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+    recorder := httptest.NewRecorder()
+    
+    TemplateWith(recorder, request)
+    
+    response := recorder.Result()
+    body, _ := io.ReadAll(response.Body)
+    fmt.Println(string(body))
+}
